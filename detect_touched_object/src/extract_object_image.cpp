764c3d1a260e86efcc0daafe93a615bae5ad96aa
au@leus <au@jsk.imi.i.u-tokyo.ac.jp>
2014-06-30 12:05:28 +0900

    update

--- detect_touched_object/src/extract_object_image.cpp
+++ detect_touched_object/src/extract_object_image.cpp
@@ -5,20 +5,17 @@
 #include <tf/transform_listener.h>
 #include <boost/foreach.hpp>
 #include <sensor_msgs/image_encodings.h>
-
-#include <jsk_pcl_ros/BoundingBoxArray.h>
-#include <jsk_pcl_ros/BoundingBox.h>
+#include <sstream>
 
 class FrameDrawer
 {
   ros::NodeHandle nh_;
-  ros::Subscriber boxs_sub_;
   image_transport::ImageTransport it_;
   image_transport::CameraSubscriber image_sub_;
   image_transport::Publisher image_pub_;
   tf::TransformListener tf_listener_;
   image_geometry::PinholeCameraModel cam_model_;
-  std::vector<std::string> frame_ids_;
+  //std::vector<std::string> frame_ids_;
   CvFont font_;
   
   
@@ -26,27 +23,81 @@ public:
   FrameDrawer()
     : it_(nh_)
   {
+    std::cout<<"framedrawer created"<<std::endl;
     std::string image_topic = nh_.resolveName("image");
-    boxs_sub_ = nh_.subscriber<jsk_pcl_ros::BoundingBoxArray>("/cluster_decomposer_final/boxes", 1, &FrameDrawer::boundingBoxCb);
-    image_sub_ = it.subscribeCamera(image_topic, 1, &FrameDrawer::imageCb, this);
+    image_sub_ = it_.subscribeCamera(image_topic, 1, &FrameDrawer::imageCb, this);
     image_pub_ = it_.advertise("image_out",1);
     cvInitFont(&font_, CV_FONT_HERSHEY_SIMPLEX, 0.5, 0.5);
   }
 
-  void boundingBoxCb(const jsk_pcl_ros::BoundingBoxArray& msg)
-  {
-    jsk_pcl_ros::BoundingBox *boxes = msg->boxes;
-  }
 
   void imageCb(const sensor_msgs::ImageConstPtr& image_msg,
                const sensor_msgs::CameraInfoConstPtr& info_msg)
   {
+    cv::Mat image;
+    cv_bridge::CvImagePtr input_bridge;
+    try {
+      input_bridge = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+      image = input_bridge->image;
+    }
+    catch (cv_bridge::Exception& ex){
+      ROS_ERROR("[draw_frames] Failed to convert image");
+      return;
+    }
+    cam_model_.fromCameraInfo(info_msg);
+
+    tf::StampedTransform transform;
+    int c = 0;
+    std::string frame_id;
+    while( c < 3)
+      {
+	std::stringstream ss;
+	 if(c < 10)
+	   ss<<"/cluster_decomposer_finaloutput0"<< c;
+	 else
+	   ss<<"/cluster_decomposer_finaloutput"<< c;
+	 
+	 frame_id = ss.str();
+	 std::cout<<frame_id<<std::endl;
+	 try {
+	   ros::Time acquisition_time = info_msg->header.stamp;
+	   ros::Duration timeout(1.0 / 30);
+	   tf_listener_.waitForTransform(cam_model_.tfFrame(), frame_id,
+					 acquisition_time, timeout);
+	   tf_listener_.lookupTransform(cam_model_.tfFrame(), frame_id,
+					acquisition_time, transform);
+	 }
+	 
+	 catch (tf::TransformException& ex) {
+	   ROS_WARN("[draw_frames] TF exception:\n%s", ex.what());
+	   return;
+	 }
+	 
+	 tf::Point pt = transform.getOrigin();
+	 if(pt.z() < 3.0){
+	 cv::Point3d pt_cv(pt.x(), pt.y(), pt.z());
+	 cv::Point2d uv;
+	 uv = cam_model_.project3dToPixel(pt_cv);
+	 
+	 static const int RADIUS = 5;
+	 cv::circle(image, uv, RADIUS, CV_RGB(255,0,0), -1);
+	 CvSize text_size;
+	 int baseline;
+	 cvGetTextSize(frame_id.c_str(), &font_, &text_size, &baseline);
+	 CvPoint origin = cvPoint(uv.x - text_size.width / 2,
+				  uv.y - RADIUS - baseline);
+	 //cv::putText(image, frame_id.c_str(), origin, cv::FONT_HERSHEY_SIMPLEX, 1, CV_RGB(255,0,0));
+	 }
+	 c++;
+      }
+    image_pub_.publish(input_bridge->toImageMsg());
   }
+
 };
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "extract_object_image");
-  FrameDrawer drawer();
+  FrameDrawer drawer;
   ros::spin();
 }
